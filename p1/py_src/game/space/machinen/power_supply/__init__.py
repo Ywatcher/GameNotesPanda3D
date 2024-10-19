46c2d2846f57a7fefa98ee3f832f212450e3f018
Ywatcher <201300030@smail.nju.edu.cn>
2024-10-20 00:13:37 +0800

    fixed import

---
+++ p1/py_src/game/space/machinen/power_supply/__init__.py
@@ -0,0 +1,10 @@
+from game.space.machinen.power_supply.electricity_appliance import ElectricityAppliance
+from game.space.machinen.power_supply.power_generator import PowerGen
+from game.space.machinen.power_supply.power_supply_system import PowerSupplySystem
+
+
+__all__ = [
+    "ElectricityAppliance",
+    "PowerGen",
+    "PowerSupplySystem"
+]
