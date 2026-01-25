88a4e9136ced981ee3b312f38403d46c3c2eede6
ywatcher <tianchi.y@wustl.edu>
2026-01-25 01:47:45 +0000

    rewrite import in main.py to match repo structure

--- p1/py_src/main.py
+++ p1/py_src/main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8-*-
 
 from direct.showbase.ShowBase import ShowBase
-from util.app import ContextShowBase
+# from util.app import ContextShowBase
+from panda3d_game.app import ContextShowBase
 from direct.task import Task
 from panda3d.core import (
     Geom,
@@ -14,7 +15,8 @@ from panda3d.core import (
 )
 import numpy as np
 import torch
-from geom.basic import create_cube_node, create_sphere_node, uv_curve_surface
+# from geom.basic import create_cube_node, create_sphere_node, uv_curve_surface
+from art.basic import create_sphere_node, create_cube_node,uv_curve_surface
 import builtins
 
 
